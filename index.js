3a4b14536499e629df1635f3c597b3cb5a5d3c5a
Rizan Mohomed <musthafarizan@gmail.com>
2018-09-27 12:02:22 +0530

    create a basic backend server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoos = require('mongoose');
+
+const app = express();
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('index page view');
+});
+
+app.get('/api', (req, res) => {
+  res.send('api documantaion');
+});
+
+const PORT = process.env.PORT || 3300;
+
+app.listen(PORT, () => console.log(`app listing on port ${PORT}...`));
+
+async function connectDB() {
+  try {
+    await mongoos.connect(
+      'mongodb://localhost/myqa',
+      { useNewUrlParser: true }
+    );
+    console.log('connected to database successfully');
+  } catch (ex) {
+    console.error('database connection error...', ex.message);
+  }
+}
